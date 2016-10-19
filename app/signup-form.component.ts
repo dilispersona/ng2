22ecaf89ebed05341f1963f5414dd923169c716a
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-10-19 11:21:56 +0530

    Using formBuilder

--- app/signup-form.component.ts
+++ app/signup-form.component.ts
@@ -1,15 +1,25 @@
 import {Component} from 'angular2/core';
-import {ControlGroup, Control, Validators} from 'angular2/common';
+import {ControlGroup, Control, Validators, FormBuilder} from 'angular2/common';
 
 @Component({
     selector: 'signup-form',
     templateUrl: 'app/signup-form.component.html'
 })
 export class SignUpFormComponent {
-    form = new ControlGroup({
+ /*   form = new ControlGroup({
          username : new Control('', Validators.required),
          password: new Control('', Validators.required)
-    });
+    });*/
+
+    //using FormBuilder
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder){
+        this.form =  fb.group({
+            username: ['',Validators.required],
+            password: ['', Validators.required]
+        })
+    }
 
     signup(){
         console.log(this.form.value);
