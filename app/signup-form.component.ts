19175e98e9f087a9e3cc42347459d7e5a423fe7e
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-10-20 10:24:55 +0530

    form validations

--- app/signup-form.component.ts
+++ app/signup-form.component.ts
@@ -1,5 +1,6 @@
 import {Component} from 'angular2/core';
 import {ControlGroup, Control, Validators, FormBuilder} from 'angular2/common';
+import { UsernameValidators } from './usernameValidators';
 
 @Component({
     selector: 'signup-form',
@@ -16,7 +17,7 @@ export class SignUpFormComponent {
 
     constructor(fb: FormBuilder){
         this.form =  fb.group({
-            username: ['',Validators.required],
+            username: ['',Validators.compose([Validators.required, UsernameValidators.cannotContainSpace])],
             password: ['', Validators.required]
         })
     }
