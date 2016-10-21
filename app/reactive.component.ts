cb5fc363f943c538e83310534a5c97a4654dde31
Dileep Cn <dileep.cn@valoremconsulting.com>
2016-10-21 13:19:27 +0530

    Reactive components, asynchronous

---
+++ app/reactive.component.ts
@@ -0,0 +1,49 @@
+//this file is intended to reference  samples using reactivex.io Components and operators , requires a differnt setup to work 
+
+import {Component} from 'angular2/core';
+import {Observable} from 'rxjs/Rx';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <input id="search" type="text" class="form-control">
+    `
+})
+export class AppComponent {
+    constructor(){
+ 	
+	var keyups = Observable.fromEvent($('#search'),'keyup')
+							.map(e => e.target.value)
+							.filter(text => text.length >= 4)
+							.debounceTime(500)
+							.distinctUntilChanged()
+							.flatMap(searchTerm => {
+									var url = "https://api.spotify.com/v1/search?type=artist&q=" + searchTerm;
+									var promise = $.getJSON(url);
+									return Observable.fromPromise(promise);									
+							});
+	
+	var subscription = keyups.subscribe(data => console.log(data));
+	//subscription.unsubscribe();
+
+
+	
+   /* 	var debounced = _.debounce(function(text){
+    		var url = "https://api.spotify.com/v1/search?type=artist&q=" + text;
+    		$.getJSON(url, function(artists){
+    			console.log(artists);
+    		});
+    	},400);
+
+    	$("#search").keyup(function(e){
+    		var text = e.target.value;
+
+    		if(text.length < 3)
+    			return;
+    		debounced(text);
+    	});*/
+
+
+
+    }
+}
